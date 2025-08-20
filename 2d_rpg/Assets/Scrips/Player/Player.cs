b386d6d5d8667554081cb4f83fffe4a99226676b
xi0824 <141408400+xi0824@users.noreply.github.com>
2025-08-20 13:41:02 +0800

    8.20

--- 2d_rpg/Assets/Scrips/Player/Player.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class Player : MonoBehaviour
-{
-    [Header("配置")]
-    [SerializeField] private PlayerStats stats;
-    public PlayerStats Stats => stats;
-    //一个用来传递数据的公共值
-}
